c18bf03082492cad023d7c3652f7dda43e66b0be
evooo62 <evren622@gmail.com>
2023-08-23 23:51:20 +0300

    Projenin katmanları oluşturuldu. Dummy data ile deneme yapıldı

---
+++ Core/ETicaretAPI.Application/Abstractions/IProductService.cs
@@ -0,0 +1,8 @@
+using ETicaretAPI.Domain.Entities;
+
+namespace ETicaretAPI.Application.Abstractions;
+
+public interface IProductService
+{
+    List<Product> GetProducts();
+}
---
+++ Core/ETicaretAPI.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,7 @@
+namespace ETicaretAPI.Domain.Entities.Common;
+
+public class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+}
